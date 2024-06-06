4dcdf859bec9221a5f0b4c5d9a8cda2722180892
Vineeth Paramathmuni <vineeth2546@gmail.com>
2024-06-06 11:29:57 +0530

    initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id: '1',
+        question: "feihiei",
+        answer: "ehfiesdhi"
+    },
+    {
+        id: '2',
+        question: "feihiei",
+        answer: "ehfiesdhi"
+    },
+    {
+        id: '3',
+        question: "feihiei",
+        answer: "ehfiesdhi"
+    },
+    {
+        id: '4',
+        question: "feihiei",
+        answer: "ehfiesdhi"
+    }
+]
